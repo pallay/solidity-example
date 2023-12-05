d643896f2938580e8b969aaede1a982494fa27f7
Pallay <pallay.raunu@gmail.com>
2023-12-05 10:24:39 +0000

    refactored create box

--- AmazonFactory.sol
+++ AmazonFactory.sol
@@ -21,27 +21,23 @@ contract AmazonFactory {
 
     Box[] public boxes;
 
-    // Create a box of size @_size and barcode @_barcode
+    // Add box to boxes
     function _addBoxToBoxes(Box memory box) private {
         boxes.push(box);
     }
 
-    // Create a box of size @_size and barcode @_barcode
-    function _createBox(string memory _size, uint _barcode) private pure returns (Box memory) {
-        return Box(_size, _barcode);
+    // Create a box of size @_size
+    function _createBox(string memory _size) private returns (Box memory) {
+        uint randomBarcode = _generateRandomBarcode(_size);
+        Box memory box = Box(_size, randomBarcode);
+        emit BoxCreated(box);
+        return box;
     }
  
     // Generate a barcode of length @barcodeLength
     function _generateRandomBarcode(string memory _str) public view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % barcodeModulus;
-    }
-
-    // Create a sized box of size @_size
-    function createSizedBox(string memory _size) public {
-        uint randomBarcode = _generateRandomBarcode(_size);
-        Box memory box = _createBox(_size, randomBarcode);
-        emit BoxCreated(box);
     }
     
 }
