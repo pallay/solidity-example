ec3f1c6ba5f8afd293fa6c1595850336eb434e80
Pallay <pallay.raunu@gmail.com>
2023-12-05 10:18:32 +0000

    emit create box event

--- AmazonFactory.sol
+++ AmazonFactory.sol
@@ -4,12 +4,16 @@ contract AmazonFactory {
 
     /** 
      * AmazonFactory
-     * @dev prints Hello World String
+     * @dev create a box
      */
     
     uint barcodeLength = 16;
     uint barcodeModulus = 10 ** barcodeLength;
 
+    // declare events
+    event BoxCreated(Box x);
+
+    // box object
     struct Box {
         string size;
         uint code;
@@ -17,18 +21,27 @@ contract AmazonFactory {
 
     Box[] public boxes;
 
-    function _createBox(string memory _size, uint _code) private {
-        boxes.push(Box(_size, _code));
+    // Create a box of size @_size and barcode @_barcode
+    function _addBoxToBoxes(Box memory box) private {
+        boxes.push(box);
+    }
+
+    // Create a box of size @_size and barcode @_barcode
+    function _createBox(string memory _size, uint _barcode) private pure returns (Box memory) {
+        return Box(_size, _barcode);
     }
  
+    // Generate a barcode of length @barcodeLength
     function _generateRandomBarcode(string memory _str) public view returns (uint) {
         uint rand = uint(keccak256(abi.encodePacked(_str)));
         return rand % barcodeModulus;
     }
 
+    // Create a sized box of size @_size
     function createSizedBox(string memory _size) public {
         uint randomBarcode = _generateRandomBarcode(_size);
-        _createBox(_size, randomBarcode);
+        Box memory box = _createBox(_size, randomBarcode);
+        emit BoxCreated(box);
     }
     
 }
